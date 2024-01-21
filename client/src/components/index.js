94b315417d649399915d589294ce579f8ca726c3
Whillian-Sousas <whillscf@gmail.com>
2024-01-20 23:49:35 -0300

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,14 @@
+import AIPicker from './AIPicker'
+import ColorPicker from './ColorPicker'
+import CustomButton from './CustomButton'
+import FilePicker from './FilePicker'
+import Tab from './Tab'
+
+
+export { 
+  Tab, 
+  CustomButton,
+  FilePicker,
+  ColorPicker,
+  AIPicker
+}
